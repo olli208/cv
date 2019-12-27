d814960b632fb90f3751711aa0dc31340b32a593
Oliver <oliveroenang@gmail.com>
2019-12-27 16:51:05 +0100

    test

--- src/components/DateRange.jsx
+++ src/components/DateRange.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import FlexWrapper from './FlexWrapper'
 
+// add date parser for date objects ?
 function DateRange({ startDate, endDate, endDateMessage = '-' }) {
 	return (
 		<FlexWrapper>
